928a98a02ac861e9d7938411c589bb99bbb219c0
Christian <cedenoc181@gmail.com>
2023-08-14 19:57:15 -0400

    finished git migration tables?

--- example-project/db/migrate/20230814233734_create_publishes.rb
+++ example-project/db/migrate/20230814233734_create_publishes.rb
@@ -1,7 +1,11 @@
 class CreatePublishes < ActiveRecord::Migration[7.0]
   def change
     create_table :publishes do |t|
-
+      t.string :date
+      t.string :category
+      t.string :author 
+      t.integer :admin_id
+      t.integer :blog_id
       t.timestamps
     end
   end
