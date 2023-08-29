5e0b8695bbb5930a5b89534e1e767d128576e5a7
Christian <cedenoc181@gmail.com>
2023-08-28 20:42:25 -0400

    dropped adminn scaffold

--- example-project/app/models/admin.rb
+++
@@ -1,4 +0,0 @@
-class Admin < ApplicationRecord
-    has_many :publishes, dependent: :destroy
-    has_many :blogs, through: :publishes
-end
--- example-project/app/models/user.rb
+++ example-project/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
     has_many :expressions, dependent: :destroy
+    has_many :publishes, dependent: :destroy
     has_many :blogs, through: :expressions
 
     has_secure_password
-    
+
     validates :email, uniqueness: true
     validates :username, uniqueness: true
 end
