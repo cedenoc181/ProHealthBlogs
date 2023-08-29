e9fe7bca7ac17ff0d505438245fa774b126ec4ac
Christian <cedenoc181@gmail.com>
2023-08-28 20:19:23 -0400

    added authentication for user login

--- example-project/app/models/user.rb
+++ example-project/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :expressions, dependent: :destroy
     has_many :blogs, through: :expressions
+
+    has_secure_password
+    
+    validates :email, uniqueness: true
+    validates :username, uniqueness: true
 end
